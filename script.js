3e12b4fbb4d15aa6532c56aaea67a7d24dfbfe16
RodrigoPessoaDev <rodrigopessoa.dev@gmail.com>
2023-10-21 16:08:40 +0100

    Create game, implementing rounds

--- script.js
+++ script.js
@@ -1,8 +1,65 @@
 //Defining variable for game options and creating a function that outputs a random computer choice
-let choice = ["rock","paper","scissors"];
+let choice = ["Rock","Paper","Scissors"];
+let playerSelection = prompt("Rock, Paper or Scissors?");
+const computerSelection = getComputerChoice(choice);
+let playerCount = 0;
+let computerCount = 0;
 
 function getComputerChoice (choice){
     return choice [Math.floor(Math.random()*choice.length)];
 }
 
-console.log(getComputerChoice(choice));
+function playRound(){
+    
+    if(playerSelection === "Rock" && computerSelection === "Scissors" ||
+       playerSelection === "Scissors" && computerSelection === "Paper"||
+       playerSelection === "Paper" && computerSelection === "Rock"){
+        return "Player wins!" + " " + playerSelection + " " + "beats" + " " + computerSelection;
+    } else if (playerSelection === computerSelection){
+        return "It\'s a tie!";
+    } else {
+        return "Computer wins!" + " " + computerSelection + " " + "beats" + " " + playerSelection;
+    }
+}
+
+function game(){
+    let round = 0;
+
+    console.log(playRound());{
+    round = ++round;
+    if (playRound()==="Player wins!" + " " + playerSelection + " " + "beats" + " " + computerSelection){
+        playerCount = ++playerCount;
+    } else if ( playRound()==="Computer wins!" + " " + computerSelection + " " + "beats" + " " + playerSelection){
+        computerCount = ++computerCount;
+    } else {
+        playerCount = ++playerCount;
+        computerCount = ++computerCount;
+    }
+    console.log(round);
+    console.log("Player:"+ playerCount);
+    console.log("Computer:"+ computerCount);
+}
+
+console.log(playRound());{
+    round = ++round;
+    if (playRound()==="Player wins!" + " " + playerSelection + " " + "beats" + " " + computerSelection){
+        playerCount = ++playerCount;
+    } else if ( playRound()==="Computer wins!" + " " + computerSelection + " " + "beats" + " " + playerSelection){
+        computerCount = ++computerCount;
+    } else {
+        playerCount = ++playerCount;
+        computerCount = ++computerCount;
+    }
+    console.log(round);
+    console.log("Player:"+ playerCount);
+    console.log("Computer:"+ computerCount);
+}
+
+    
+
+    
+
+    
+}
+
+console.log(game());
